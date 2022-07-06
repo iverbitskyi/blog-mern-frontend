c1c026bcffd52a5b36458408f8655bb350c70988
v_verbitskyi <ivan.verbitskyj@gmail.com>
2022-07-06 11:53:23 +0300

    first commit

---
+++ src/components/Header/index.jsx
@@ -0,0 +1,46 @@
+import React from 'react';
+import Button from '@mui/material/Button';
+import { Link } from 'react-router-dom';
+
+
+import styles from './Header.module.scss';
+import Container from '@mui/material/Container';
+
+export const Header = () => {
+  const isAuth = false;
+
+  const onClickLogout = () => {};
+
+  return (
+    <div className={styles.root}>
+      <Container maxWidth="lg">
+        <div className={styles.inner}>
+          <Link className={styles.logo} to="/">
+            <div>salam aleykum</div>
+          </Link>
+          <div className={styles.buttons}>
+            {isAuth ? (
+              <>
+                <Link to="/posts/create">
+                  <Button variant="contained">Написати статтю</Button>
+                </Link>
+                <Button onClick={onClickLogout} variant="contained" color="error">
+                  Вийти
+                </Button>
+              </>
+            ) : (
+              <>
+                <Link to="/login">
+                  <Button variant="outlined">Зайти</Button>
+                </Link>
+                <Link to="/register">
+                  <Button variant="contained">Створоити акаунт</Button>
+                </Link>
+              </>
+            )}
+          </div>
+        </div>
+      </Container>
+    </div>
+  );
+};
